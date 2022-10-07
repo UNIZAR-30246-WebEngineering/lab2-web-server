ada4c2ee2b2b608b08292e2bf67a7932777e2710
hector toral <hectortoraltrabajos@gmail.com>
2022-10-07 16:34:15 +0200

    Primary Task

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,15 +19,13 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
-
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-
     detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.21.0")
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+    compileOnly("org.springframework.boot:spring-boot-devtools")
 }
 
 tasks.withType<KotlinCompile> {
